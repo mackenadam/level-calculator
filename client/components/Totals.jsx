221f96b6dddeca54d44e8818fa2e67fc2819be06
mackenadam <mackenadam@gmail.com>
2021-02-12 13:42:39 +1300

    Add logic for finding total per 100g

--- client/components/Totals.jsx
+++ client/components/Totals.jsx
@@ -2,6 +2,32 @@ import React from 'react'
 import { connect } from 'react-redux'
 
 class Totals extends React.Component {
+
+  sumValues = (value) => {
+    let total = 0
+    let weight = 0
+    if (this.props.recipe.length === 0) {
+      return 0
+    } else if (this.props.recipe.length === 1) {
+      if (this.props.recipe[0][value] === 0 || this.props.recipe[0].weight === 0) {
+        return 0
+      } else {
+        return (this.props.recipe[0][value] / this.props.recipe[0].weight).toFixed(1)
+      }
+    } else {
+      if (this.props.recipe[0][value] === 0 && this.props.recipe[0].weight === 0) {
+        return 0
+      } else {
+        this.props.recipe.map(ingredient => {
+          total = ingredient[value] + total
+          weight = (ingredient.weight + weight)
+        })
+        weight = weight / 100
+        return (total / weight).toFixed(1)
+      }
+    } 
+  }
+
   render () {
     return (
       <tfoot>
@@ -14,12 +40,15 @@ class Totals extends React.Component {
           <th>Protein</th>
         </tr>
         <tr>
-          <th>Totals</th>
-          <td>Cal</td>
-          <td>Fat</td>
-          <td>Carbs</td>
-          <td>Sugar</td>
-          <td>Protein</td>
+          <th>Per 100g</th>
+        </tr>
+        <tr>
+          <th>Total</th>
+          <td>{this.sumValues('energy')}</td>
+          <td>{this.sumValues('fat')}</td>
+          <td>{this.sumValues('carbohydrates')}</td>
+          <td>{this.sumValues('sugar')}</td>
+          <td>{this.sumValues('protein')}</td>
         </tr>
       </tfoot>
     )
