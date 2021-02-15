53a6d2d31a2cafbc98561bd4d8ba52fc0af8f720
mackenadam <mackenadam@gmail.com>
2021-02-15 17:10:26 +1300

    Refactor API .js files

--- client/apis/ingredients.js
+++ client/apis/ingredients.js
@@ -2,33 +2,25 @@ import request from 'superagent'
 
 const rootUrl = '/api/v1/ingredients'
 
-export function getIngredientsAPI() {
-  return request.get(rootUrl)
-    .then(res => {
-      return res.body
-    })
-}
-
 export function addIngredientAPI(newIngredient) {
   return request.post(rootUrl + '/addIngredient')
     .send(newIngredient)
-    .then(res => {
-      return res.body
-    })
+    .then(res => res.body)
 }
 
 export function deleteIngredientAPI(id) {
   return request.post(rootUrl + '/deleteIngredient')
     .send({id})
-    .then(res => {
-      return res.body
-    })
+    .then(res => res.body)
+}
+
+export function getIngredientsAPI() {
+  return request.get(rootUrl)
+  .then(res => res.body)
 }
 
 export function updateIngredientAPI(id, ingredient) {
   return request.post(rootUrl + '/updateIngredient')
     .send({id: id, ingredient: ingredient})
-    .then(res => {
-      return res.body
-    })
+    .then(res => res.body)
 }
--- client/apis/recipe.js
+++ client/apis/recipe.js
@@ -4,31 +4,23 @@ const rootUrl = '/api/v1/recipe'
 
 export function getRecipeAPI() {
   return request.get(rootUrl)
-    .then(res => {
-      return res.body
-    })
+  .then(res => res.body)
 }
 
 export function addRecipeIngredientAPI(newIngredient) {
   return request.post(rootUrl + '/addToRecipe')
     .send(newIngredient)
-    .then(res => {
-      return res.body
-    })
+    .then(res => res.body)
 }
 
 export function removeRecipeIngredientAPI(id) {
   return request.post(rootUrl + '/removeFromRecipe')
     .send({id})
-    .then(res => {
-      return res.body
-    })
+    .then(res => res.body)
 }
 
 export function updateRecipeAPI(ingredient) {
   return request.post(rootUrl + '/updateRecipe')
   .send(ingredient)
-  .then(res => {
-    return res.body
-  })
+  .then(res => res.body)
 }
