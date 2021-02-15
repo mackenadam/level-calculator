b94e3470ceb6508de04d11f28da0c91d46b1507e
mackenadam <mackenadam@gmail.com>
2021-02-15 17:08:06 +1300

    Refactor reducer .js files

--- client/reducers/index.js
+++ client/reducers/index.js
@@ -1,11 +1,9 @@
 import { combineReducers } from 'redux'
 
-import fruits from './fruits'
 import ingredients from './ingredients'
 import recipe from './recipe'
 
 export default combineReducers({
-  fruits,
   ingredients,
   recipe
 })
