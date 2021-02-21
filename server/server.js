55c053db38411968ab459660afb7b72c5fc3c676
mackenadam <mackenadam@gmail.com>
2021-02-21 16:18:26 +1300

    Refactor existing code for initial Heroku deploy

--- server/server.js
+++ server/server.js
@@ -1,7 +1,6 @@
 const express = require('express')
 const path = require('path')
 
-const fruitRoutes = require('./routes/fruits')
 const ingredientsRoutes = require('./routes/ingredients')
 const recipeRoutes = require('./routes/recipe')
 
@@ -10,7 +9,6 @@ const server = express()
 server.use(express.json())
 server.use(express.static(path.join(__dirname, 'public')))
 
-server.use('/api/v1/fruits', fruitRoutes)
 server.use('/api/v1/ingredients', ingredientsRoutes)
 server.use('/api/v1/recipe', recipeRoutes)
 
