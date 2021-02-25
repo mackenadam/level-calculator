ff447c69cbf3f55e0323ebdc214a8bb061fc041a
mackenadam <mackenadam@gmail.com>
2021-02-25 17:21:16 +1300

    Initialise passport and passport-session

--- server/server.js
+++ server/server.js
@@ -1,14 +1,19 @@
 const express = require('express')
+const passport = require('passport')
 const path = require('path')
 
+const authRoutes = require('./routes/auth')
 const ingredientsRoutes = require('./routes/ingredients')
 const recipeRoutes = require('./routes/recipe')
 
 const server = express()
-
 server.use(express.json())
 server.use(express.static(path.join(__dirname, 'public')))
 
+server.use(passport.initialize())
+server.use(passport.session())
+
+server.use('/api/auth', authRoutes)
 server.use('/api/v1/ingredients', ingredientsRoutes)
 server.use('/api/v1/recipe', recipeRoutes)
 
